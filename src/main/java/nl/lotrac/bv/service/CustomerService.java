2b53954f84e0335ad03cf0c4c0a100f241a480df
=Rob Franke <robwfranke@gmail.com>
2021-05-01 15:55:56 +0200

    handmatig CustomerServiceImpl aangemaakt

--- src/main/java/nl/lotrac/bv/service/CustomerService.java
+++ src/main/java/nl/lotrac/bv/service/CustomerService.java
@@ -1,7 +1,7 @@
 package nl.lotrac.bv.service;
 
 import nl.lotrac.bv.model.Authority;
-import nl.lotrac.bv.model.User;
+import nl.lotrac.bv.model.Customer;
 
 import java.util.Collection;
 import java.util.Optional;
@@ -11,21 +11,22 @@ import java.util.Set;
 public interface CustomerService {
 
 
-
-    public abstract String createUser(User user);
-
-    public abstract void updateUser(String username, User user);
-
-    public abstract void deleteUser(String username);
-    public abstract Collection<User> getUsers();
-   ;
-    public abstract Optional<User> getUser(String username);
-    public abstract boolean userExists(String username);
-
-    public abstract Collection<User> getAllAuthorities();
-    public abstract Set<Authority> getAuthorities(String username);
-    public abstract void addAuthority(String username, String authority);
-    public abstract void removeAuthority(String username, String authority);
+//
+//
+//    public abstract String createUser(User user);
+//
+//    public abstract void updateUser(String username, User user);
+//
+//    public abstract void deleteUser(String username);
+//    public abstract Collection<User> getUsers();
+//   ;
+//    public abstract Optional<User> getUser(String username);
+//    public abstract boolean userExists(String username);
+//
+//    public abstract Collection<User> getAllAuthorities();
+//    public abstract Set<Authority> getAuthorities(String username);
+//    public abstract void addAuthority(String username, String authority);
+//    public abstract void removeAuthority(String username, String authority);
 
 
 }
--- src/main/java/nl/lotrac/bv/service/UserService.java
+++ src/main/java/nl/lotrac/bv/service/UserService.java
@@ -19,7 +19,7 @@ public interface UserService {
 
     public abstract void deleteUser(String username);
     public abstract Collection<User> getUsers();
-   ;
+
     public abstract Optional<User> getUser(String username);
     public abstract boolean userExists(String username);
 
